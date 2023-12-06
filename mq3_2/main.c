8b4ebf5ecc669dabaf620cb91f76eadbed1d3286
Healy <hfuess@pro1iaq.com>
2023-12-05 19:12:26 -0500

    print to stdout

--- mq3_2/main.c
+++ mq3_2/main.c
@@ -1,4 +1,8 @@
 #include <atmel_start.h>
+#include <stdio.h>
+
+static FILE mystdout = FDEV_SETUP_STREAM(USART_0_write, NULL, _FDEV_SETUP_WRITE);
+
 
 int main(void)
 {
@@ -6,9 +10,19 @@ int main(void)
 	atmel_start_init();
 
     int a = 0;
+    stdout = &mystdout;
+    
+   // static FILE mystdout = FDEV_SETUP_STREAM(USART_0_write, NULL, _FDEV_SETUP_WRITE);
+
+
     
 	/* Replace with your application code */
 	while (1) {
+       //
+       
         a = ADC_0_get_conversion(5);
+        printf("mq3 val: %d\n\r", a);
+       // USART_0_write('hello');
+       // USART_0_test_usart_basic();
     }        
 }
